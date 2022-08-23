2fd98f2d22941670d4a4860f132583df74e0228e
Heiswill <willtriger@gmail.com>
2022-08-23 14:10:14 -0700

    printLowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= chr('a') and ord(c) <= chr('z'):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
