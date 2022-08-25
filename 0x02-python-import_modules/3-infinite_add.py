e7d95de4bcfd0f67bf1c0dab4ec4f67a7ef74c87
Heiswill <willtriger@gmail.com>
2022-08-25 14:08:08 -0700

    test3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_arg(args):
+    
+    num = len(args) - 1
+    if num == 0:
+        print("{}".format(num))
+        return
+    elif num >= 2:
+        Sum = 0
+        for i in range(1, num + 1):
+            Sum += int(args[i])
+        print("{}".format(Sum))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
