684f86a2e4875ff5a56a6e8882247be16f7f697d
Heiswill <willtriger@gmail.com>
2022-08-25 15:23:27 -0700

    testa

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    from sys import argv
 
-def add_arg(args):
-    
-    num = len(args) - 1
+    num = len(argv) - 1
     if num == 0:
-        print("{}".format(num))
-        return
-    elif num >= 2:
+        print("{}".format(num)) 
+    else:
         Sum = 0
         for i in range(1, num + 1):
-            Sum += int(args[i])
+            Sum += int(argv[i])
         print("{}".format(Sum))
 
-if __name__ == "__main__":
-    add_arg(sys.argv)
+
