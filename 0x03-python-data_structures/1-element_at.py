1c42a651eb25aa95c7f3751d6793554158662b75
Heiswill <willtriger@gmail.com>
2022-08-27 17:31:16 -0700

    correctTwo

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx > len(my_list):
-        return None
-    else:
+    if 0 <= idx < len(my_list):
         return my_list[idx]
+    else:
+        return None
 
 
 if __name__ == "__main__":
