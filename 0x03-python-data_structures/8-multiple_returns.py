ca859d3ebd950c3fa777cfc13cf029300e8e28e6
Heiswill <willtriger@gmail.com>
2022-08-27 18:11:26 -0700

    len

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence[0] = None
-    return len(sentence), sentence[0]
+    if len(sentence) > 0:
+        return len(sentence), sentence[0]
+    return None
