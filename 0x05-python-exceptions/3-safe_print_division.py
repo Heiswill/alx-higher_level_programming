910feacf1d906b3db700ea4937e7fff18bf8873b
Heiswill <willtriger@gmail.com>
2022-09-12 07:17:20 -0700

    optional

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        dv = a / b
+    except ZeroDivisionError:
+        dv = None
+    finally:
+        print("Inside result: {}".format(dv))
+        return(dv)
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    dv_list = []
+    for i in range(0, list_length):
+        try:
+            dv = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            dv = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            dv = 0
+        except IndexError:
+            print("out of range")
+            dv = 0
+        finally:
+            dv_list.append(dv)
+
+    return (dv)
