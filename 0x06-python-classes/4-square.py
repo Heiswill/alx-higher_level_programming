144c5fbec767758d7f7800ce23b269311600302f
Heiswill <willtriger@gmail.com>
2022-09-13 15:20:36 -0700

    control

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,30 +3,41 @@
 
 
 class Square():
-    """Square class"""
+    """Square class for a quadrilateral with four equal sides"""
 
     def __init__(self, size=0):
-        """Initializes a new square
-
+        """Sets the size of the square on instantation
+           Throws an error if the size called with it is not an integer
         Args:
-            size (int): the size of the square"""
+            size (int, optional): The size of the square object
+        Raises:
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
+        """
 
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """return the sizeof the square"""
+        """Return the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the sizeof the square with a value"
-
+        """Sets the size of the square with a value"
         Arg: value the new size of the square
-        """
-        if not isinstance(size, int):
+        Raises:
+        TypeError: When the value passed in is not an integer
+        ValueError: When the value passed in is less than 0"""
+
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
