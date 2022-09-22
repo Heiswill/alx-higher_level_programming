1e9bd3c013ef51c6c6fda096a65be90ab656cb95
Heiswill <105241740+Heiswill@users.noreply.github.com>
2022-09-22 16:23:58 -0700

    Delete 0x1A-hash_tables directory

--- 0x1A-hash_tables/2-key_index.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "hash_tables.h"
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	return (hash_djb2(key) % size);
-}
