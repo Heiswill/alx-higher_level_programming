dc0ee0d29f9b2c7fbaaeb935f1315b97a9008907
Heiswill <willtriger@gmail.com>
2022-09-30 12:03:46 -0700

    baseClass

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a base class for other tasks."""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
