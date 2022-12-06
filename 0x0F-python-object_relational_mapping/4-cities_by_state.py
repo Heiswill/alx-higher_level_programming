22662210bb7d81c1a39847c808a4c1451b723c65
Heiswill <willtriger@gmail.com>
2022-12-06 03:54:48 -0800

    check

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+'''
+a script that lists all cities from the database hbtn_0e_4_usa.
+'''
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], database=argv[3]
+            )
+    cursor = conn.cursor()
+    cursor.execute(
+            """SELECT cities.id, cities.name, states.name
+            FROM cities LEFT JOIN states ON cities.state_id
+            = states.id ORDER BY cities.id ASC""")
+    qrows = cursor.fetchall()
+    for row in qrows:
+        print(row)
+    cursor.close()
+    conn.close()
+
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+'''
+a script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa.
+'''
+if __name__ == "__main__":
+    cont = 0
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = conn.cursor()
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    LEFT JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC""")
+    qrows = cursor.fetchall()
+    for row in qrows:
+        if row[2] == argv[4]:
+            if cont > 0:
+                print(", ", end="")
+            print(row[1], end="")
+            cont = cont + 1
+    print()
+    cursor.close()
+    conn.close()
