bc14af982f8e541f395455e89c06bc9025618e3e
Heiswill <willtriger@gmail.com>
2022-12-15 08:08:22 -0800

    Accept

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -X POST "$1" -H 'Content-Type: application/json' -d "@$2"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response.
 curl -sX DELETE "$1"
