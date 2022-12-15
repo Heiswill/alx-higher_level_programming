b621cb63116f56fb0503be7445db1117da8cb1e2
Heiswill <willtriger@gmail.com>
2022-12-15 07:56:10 -0800

    Accept

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+
+curl -X POST "$1" \
+	-H 'Content-Type: application/json' \
+	-d "@$2"
