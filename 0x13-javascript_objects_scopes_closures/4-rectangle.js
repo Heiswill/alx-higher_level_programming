ca0e48d9b396cbeb9d3a036d43479e0760a08873
Heiswill <willtriger@gmail.com>
2022-12-26 13:12:32 -0800

    script

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+	constructor (w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	print () {
+		for (let i = 0; i < this.height; i++) {
+			let prints = '';
+			for (let j = 0; j = this.width; j++) {
+				prints += 'X';
+			}
+			console.log(prints);
+		}
+	}
+	
+	rotate () {
+		let temp;
+		temp = this.height;
+		this.height = this.width;
+		this.width = temp;
+	}
+
+	double () {
+		this.height *= 2;
+		this.width *= 2;
+	}
+}
+
+module.exports = Rectangle;
+
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const S = require('./5-square');
+
+class Square extends S {
+	charPrint (c) {
+		if (typeof (c) === 'undefined') {
+			c = 'X';
+		}
+		for (let row = 0; row < this.height; row++) {
+			let prints = '';
+			for (let col = 0; col < this.width; col++) {
+				prints += c;
+			}
+			console.log(prints);
+		}
+	}
+}
+module.exports = Square;
