6c949c165ca971800031ceab0a7385055f0bf8b0
Heiswill <willtriger@gmail.com>
2023-01-17 16:55:57 -0800

    const

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,10 +9,10 @@ request(reqURL, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
   } else {
     const jso = JSON.parse(body);
-    const results = jso['results'];
+    const results = jso.results;
     let count = 0;
     for (let i = 0; i < results.length; i++) {
-      const chars = (results[i]['characters']);
+      const chars = (results[i].characters);
       for (let j = 0; j < chars.length; j++) {
         const check18 = chars[j].endsWith('18/');
         if (check18) {
